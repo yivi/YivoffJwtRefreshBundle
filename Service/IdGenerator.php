f51448e47b0aaa206a60cd4ae32bfc12346d1be9
yivi <ivan.yivoff@easyoffer.es>
2020-05-03 13:57:15 +0200

    generator for identifier and verifier

---
+++ Service/IdGenerator.php
@@ -0,0 +1,19 @@
+<?php declare(strict_types=1);
+
+namespace Yivoff\JwtTokenRefresh\Service;
+
+use Yivoff\JwtTokenRefresh\Contracts\IdGeneratorInterface;
+
+class IdGenerator implements IdGeneratorInterface
+{
+
+    public function generateIdentifier(): string
+    {
+        return hash('md5', random_bytes(9));
+    }
+
+    public function generateVerifier(): string
+    {
+        return hash('sha256', random_bytes(12));
+    }
+}
