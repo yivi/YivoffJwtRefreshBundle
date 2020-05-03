a69a0f1f9c964b2e879ff51055ddfeff075488fa
yivi <ivan.yivoff@easyoffer.es>
2020-05-03 13:59:14 +0200

    event listener to attach the refresh token to the authentication token. all tokens are used only once, so if a previous token exists it gets deleted.

---
+++ EventListener/AttachRefreshToken.php
@@ -0,0 +1,62 @@
+<?php declare(strict_types=1);
+
+namespace Yivoff\JwtTokenRefresh\EventListener;
+
+use DateInterval;
+use DateTimeImmutable;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Yivoff\JwtTokenRefresh\Contracts\EncoderInterface;
+use Yivoff\JwtTokenRefresh\Contracts\IdGeneratorInterface;
+use Yivoff\JwtTokenRefresh\Contracts\RefreshTokenProviderInterface;
+use Yivoff\JwtTokenRefresh\Model\RefreshToken;
+
+final class AttachRefreshToken
+{
+
+    private RefreshTokenProviderInterface  $tokenProvider;
+    private EncoderInterface $encoder;
+    private IdGeneratorInterface $idGenerator;
+
+    private string $parameterName;
+    private int    $tokenShelfLife;
+
+    public function __construct(
+        EncoderInterface $encoder,
+        IdGeneratorInterface $idGenerator,
+        string $parameterName,
+        int $tokenShelfLife,
+        RefreshTokenProviderInterface $tokenProvider
+    ) {
+        $this->tokenProvider  = $tokenProvider;
+        $this->encoder        = $encoder;
+        $this->idGenerator    = $idGenerator;
+        $this->parameterName  = $parameterName;
+        $this->tokenShelfLife = $tokenShelfLife;
+    }
+
+    public function __invoke(AuthenticationSuccessEvent $event)
+    {
+        /** @var UserInterface $user */
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (null !== $oldToken = $this->tokenProvider->getTokenForUsername($user->getUsername())) {
+            $this->tokenProvider->deleteTokenWithIdentifier($oldToken->getIdentifier());
+        }
+
+        $identifier   = $this->idGenerator->generateIdentifier();
+        $raw_verifier = $this->idGenerator->generateVerifier();
+
+        $token = new RefreshToken(
+            $user->getUsername(),
+            $identifier,
+            $this->encoder->encode($raw_verifier),
+            (new DateTimeImmutable())->add(new DateInterval('PT' . $this->tokenShelfLife . 'S'))
+        );
+        $this->tokenProvider->add($token);
+
+        $data[$this->parameterName] = $token->getIdentifier() . ':' . $raw_verifier;
+        $event->setData($data);
+    }
+}
