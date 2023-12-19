ec5b09d5c374667ffbf70868057d737b706c4bba
yivi <ivan@yivoff.com>
2023-12-19 13:16:08 +0100

    MISC: Update dev tools. Remove Bundle dependency from bundle's bundle class

--- src/YivoffJwtRefreshBundle.php
+++ src/YivoffJwtRefreshBundle.php
@@ -4,9 +4,55 @@ declare(strict_types=1);
 
 namespace Yivoff\JwtRefreshBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
+use Yivoff\JwtRefreshBundle\DependencyInjection\YivoffJwtRefreshExtension;
+use function dirname;
 
-class YivoffJwtRefreshBundle extends Bundle
+class YivoffJwtRefreshBundle implements BundleInterface
 {
     public const BUNDLE_PREFIX = 'yivoff_jwt_refresh';
+
+    protected ExtensionInterface|false|null $extension = null;
+    protected ?ContainerInterface $container = null;
+
+    public function boot()
+    {
+    }
+
+    public function shutdown()
+    {
+    }
+
+    public function build(ContainerBuilder $container)
+    {
+    }
+
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new YivoffJwtRefreshExtension();
+    }
+
+    public function getName(): string
+    {
+        return 'YivoffJwtRefreshBundle';
+    }
+
+    public function getNamespace(): string
+    {
+        return 'Yivoff';
+    }
+
+    public function getPath(): string
+    {
+        return dirname(__DIR__);
+    }
+
+    public function setContainer(?ContainerInterface $container): void
+    {
+        $this->container = $container;
+    }
+
 }
